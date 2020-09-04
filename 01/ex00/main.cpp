5e590e2b90a4b717868b696dd3f6e24fb93cb5ee
Arseniy SHISHAEV <ashishae@student.42.fr>
2020-09-04 17:44:40 +0200

    Update

--- 01/ex00/main.cpp
+++ 01/ex00/main.cpp
@@ -6,23 +6,27 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 19:28:11 by ashishae          #+#    #+#             */
-/*   Updated: 2020/08/24 13:38:29 by ashishae         ###   ########.fr       */
+/*   Updated: 2020/09/04 17:42:20 by ashishae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Pony.hpp"
 
-// createStackPony will create a Pony on a stack
-// and try to return its address
-Pony *createStackPony(void)
+// ponyOnTheStack will create a Pony on a stack
+void	ponyOnTheStack(void)
 {
 	Pony stackPony;
-	return &stackPony;
+	Pony *pointerToStackPony;
+
+	pointerToStackPony = &stackPony;
+
+	std::cout << "Address of stackPony is: " << pointerToStackPony
+	<< std::endl;
 }
 
-// createStackPony will create a 
+// ponyOnTheHeap will create a 
 // dynamically allocated pony
-Pony *createHeapPony(void)
+Pony *ponyOnTheHeap(void)
 {
 	Pony *heapPony = new Pony;
 	return heapPony;
@@ -30,13 +34,11 @@ Pony *createHeapPony(void)
 
 int main(void)
 {
-	Pony *heapPony = createHeapPony();
+	Pony *heapPony = ponyOnTheHeap();
 	std::cout << "Address of heapPony is: " << heapPony
 	<< std::endl;
 	delete heapPony;
 
-	Pony *pointerToStackPony = createStackPony();
-	// pointerToStackPony = &stackPony;
-	std::cout << "Address of stackPony is: " << pointerToStackPony
-	<< std::endl;
+	ponyOnTheStack();
+	std::cout << "Stack pony will not exist by this point" << std::endl;
 }
